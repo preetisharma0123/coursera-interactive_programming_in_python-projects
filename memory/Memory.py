202352f8ba4cdf59b16a39246c037497e4921611
Preeti Sharma <preetisharma0123@gmail.com>
2023-10-05 16:29:16 -0300

    Immplemented the logic of the mini project Memory -README.md added -demo added -code logic added

--- memory/Memory.py
+++ memory/Memory.py
@@ -3,22 +3,67 @@
 import simplegui
 import random
 
-# helper function to initialize globals
-def new_game():
-    pass  
+list1 = range(0,8)
+list2 = range(0,8)
+State = 0
+cards = list1+list2
+random.shuffle(cards)
+card_pos = [0,80]
+exposed = [ False, False,False,False,False,False,False,False,False,False,False,False,False,False,False,False]
+turns = 0
 
-     
+# helper function to initialize globals
+def new_game():     
+    global state,exposed,turns,cards
+    state = 0
+    turns = 0
+    exposed = [ False, False,False,False,False,False,False,False,False,False,False,False,False,False,False,False]
+    random.shuffle(cards)
+    
 # define event handlers
 def mouseclick(pos):
     # add game state logic here
-    pass
+    global card_pos,cards,state,card_index1,card_index2,turns
+   
+    if state == 0:
+        state = 1 
+        turns += 1
+        card_index1 = pos[0] // 50
+        if not(exposed[card_index1]):
+            exposed[card_index1] = True
+        print "card_index1: ",card_index1
+        
+    elif state == 1:
+        state = 2
+        card_index2 = pos[0] // 50
+        if not(exposed[card_index2]):
+            exposed[card_index2] = True
+        print "card_index2: ",card_index2
+         
+    elif state ==2:
+        if cards[card_index1] != cards[card_index2]:
+                exposed[card_index1] =  False
+                exposed[card_index2] =  False
+                    
+        state = 1
+        turns +=1
+        card_index1 = pos[0] // 50
+        if not(exposed[card_index1]):
+            exposed[card_index1] = True
+        print "card_index1: ", card_index1
     
-                        
+
 # cards are logically 50x100 pixels in size    
 def draw(canvas):
-    pass
-
-
+    global position,cards,card_pos,exposed
+    
+    for card_index in range(len(cards)):   
+        card_pos[0] =50*card_index
+        if exposed[card_index]:
+            canvas.draw_text(str(cards[card_index]),card_pos,80,"white")                
+        else:
+            canvas.draw_polygon([[card_pos[0],card_pos[1]-100],[card_pos[0]+50, card_pos[1]-100],[card_pos[0]+50,card_pos[1]+100],[card_pos[0],card_pos[1]+50]],1, 'black', 'green')
+    label.set_text('"Turns = '+str(turns)+'"')          
 # create frame and add a button and labels
 frame = simplegui.create_frame("Memory", 800, 100)
 frame.add_button("Reset", new_game)
@@ -32,5 +77,3 @@ frame.set_draw_handler(draw)
 new_game()
 frame.start()
 
-
-# Always remember to review the grading rubric
---
+++ memory/README.md
@@ -0,0 +1,10 @@
+## Memory
+Memory is a card game in which the player deals out a set of cards face down. In Memory, a turn (or a move) consists of the player flipping over two cards. If they match, the player leaves them face up. If they don't match, the player flips the cards back face down. The goal of Memory is to end up with all of the cards flipped face up in the minimum number of turns. For this project, we will keep our model for Memory fairly simple. A Memory deck consists of eight pairs of matching cards.
+
+## Lesson learn
+- Read mouse input, learn about list methods and dictionaries, draw images
+
+## Demo
+[]()->[CodeSkulptor link for active execution](https://py2.codeskulptor.org/#user50_mDxTFBDLaI_19.pyt)
+![](./demo.gif)
+
